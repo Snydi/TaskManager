e47c2022b3b7035995290d7911dff3ce327e3ef6
Dmitriy Snyatkov <d-snyatkov@mail.ru>
2022-11-09 22:45:36 +0300

    added remove from bd function(doesnt work yet)

--- classes/AuthManager.php
+++ classes/AuthManager.php
@@ -4,6 +4,7 @@ class AuthManager
 {
     private string $email;
     private string $password;
+    private string $id;
     public function __construct($email,$password)
     {
         $this->email = $email;
@@ -15,8 +16,15 @@ class AuthManager
             $query = "INSERT INTO users (id, email, password) VALUES (NULL, '$this->email', '$this->password') ";
             mysqli_query(Database::connection(), $query) or die(mysqli_error(Database::connection()));
         }
+    public function removeUserFromDB()
+    {
+        $query = "DELETE FROM users WHERE id = $this->id";
+    }
 
-
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
 
 
 }
--- logic/authHandler.php
+++ logic/authHandler.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../classes/AuthManager.php';
+
 if(isset($_POST["submit"]))
 {
     $user = new AuthManager($_POST["email"], $_POST["password"]);
